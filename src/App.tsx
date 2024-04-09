f57b68c6c534f7423c39e76e28970630ab6c5b46
hieutvIcetea <hieu.tran@icetea.io>
2024-04-09 18:07:43 +0700

    init

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import { useEffect, useState } from "react";
 import "./App.css";
 import { Button } from "./components/ui/button";
 import toast, { Toaster } from "react-hot-toast";
-import { MainButton, init, retrieveLaunchParams } from "@tma.js/sdk";
+import { MainButton, init } from "@tma.js/sdk";
 // import WebAppUser from "@twa-dev/sdk";
 // import { webApp } from "node_modules/telegraf/typings/button";
 // import WheelComponent from "./components/Wheel";
@@ -53,7 +53,7 @@ function App() {
     miniApp.ready();
   }, []);
 
-  const { initData } = retrieveLaunchParams();
+  // const { initData } = retrieveLaunchParams();
 
   useEffect(() => {
     if (question === 3) {
@@ -100,7 +100,7 @@ function App() {
       /> */}
       <div className="flex flex-col gap-6">
         {/* {JSON.stringify(converted)} */}
-        {JSON.stringify(initData)}
+        {/* {JSON.stringify(initData)} */}
         {question < 3 && (
           <>
             <h1>{QUIZ.questions[question].question}</h1>
