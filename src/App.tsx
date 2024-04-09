a1701246ec2751fcf47b43950978064ae44ac33a
hieutvIcetea <hieu.tran@icetea.io>
2024-04-09 11:04:12 +0700

    init

---
+++ src/App.tsx
@@ -0,0 +1,69 @@
+import { useState } from "react";
+import "./App.css";
+import { Button } from "./components/ui/button";
+
+const QUIZ = {
+  quiz_title: "Science Quiz",
+  questions: [
+    {
+      question: "What is the chemical symbol for water?",
+      options: ["H2O", "CO2", "NaCl", "O2"],
+      answer: "H2O",
+    },
+    {
+      question: "What is the powerhouse of the cell?",
+      options: ["Nucleus", "Mitochondria", "Ribosome", "Endoplasmic reticulum"],
+      answer: "Mitochondria",
+    },
+    {
+      question: "Which planet is known as the Red Planet?",
+      options: ["Jupiter", "Mars", "Venus", "Saturn"],
+      answer: "Mars",
+    },
+  ],
+};
+
+function App() {
+  const [question, setQuestion] = useState(0);
+
+  const submitAnswer = (answer: string) => {
+    if (QUIZ.questions[question].answer === answer) {
+      alert("correct");
+      setQuestion(question + 1);
+      return;
+    }
+
+    alert("wrong");
+  };
+
+  return (
+    <div className="flex items-center justify-center mt-20">
+      <div className="flex flex-col gap-6">
+        {question < 3 && (
+          <>
+            <h1>{QUIZ.questions[question].question}</h1>
+            {QUIZ.questions[question].options.map((item) => (
+              <Button
+                className="px-3 py-2 border rounded w-fit bg-slate-300 hover:bg-emerald-400"
+                onClick={() => submitAnswer(item)}
+              >
+                {item}
+              </Button>
+            ))}
+          </>
+        )}
+        {question === 3 && <h1>Congrats!</h1>}
+        {/* {QUIZ.questions.map((item) => (
+          <Button
+            className="px-3 py-2 border rounded w-fit bg-slate-300 hover:bg-emerald-400"
+            onClick={setAnswer(item.)}
+          >
+            {item.question}
+          </Button>
+        ))} */}
+      </div>
+    </div>
+  );
+}
+
+export default App;
